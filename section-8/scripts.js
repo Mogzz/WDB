127ae953f4fdf18d94cba48a92eaa580405eab8c
Mogzz <mparry92@gmail.com>
2018-01-18 14:33:05 +0000

    Add section-8

---
+++ section-8/scripts.js
@@ -0,0 +1,11 @@
+alert("Time to Play a Game!");
+
+var firstName = prompt("Please Enter Your First name");
+var lastName = prompt("Please Enter Your Last Name");
+var age = prompt("Please Enter Your Age");
+
+alert("Hello! " + firstName + " " + lastName);
+alert("You are " + age + ", Old as fuck bruh!");
+
+
+alert("That means you are " + age * 365 + " days old... SHIIIIIIIIT!");
