36dbdb75cf8d83e359499c29d4f70e92859c52b8
Mogzz <mparry92@gmail.com>
2018-01-21 11:08:17 +0000

    update!

--- section-11/todo.js
+++ section-11/todo.js
@@ -3,15 +3,38 @@ var todos = [];
 while (input !== "quit") {
 
   if (input === "list") {
-    console.log(todos);
+    listToDos();
   } else if (input === "new") {
-    //ask for todos with prompt 
-    var newToDo = prompt("Enter new todo");
-    //add to array with push
-    todos.push(newToDo);
+    addToDo();
+  } else if (input === "delete") {
+    deleteToDo();
   }
 
   input = prompt("Please enter what you would like to do");
 }
 
 console.log("Ok, quit the app!");
+
+function listToDos() {
+  todos.forEach(function(todo, index) {
+    console.log("**********");
+    console.log(index + ": " + todo);
+    console.log("**********");
+  });
+}
+
+function addToDo() {
+  //ask for todos with prompt
+  var newToDo = prompt("Enter new todo");
+  //add to array with push
+  todos.push(newToDo);
+  console.log("Added Task");
+}
+
+function deleteToDo() {
+  //ask for index to be deleted
+  var index = prompt("Enter index of task to delete");
+  //delete the index
+  todos.splice(index, 1);
+  console.log("Deleted task");
+}
